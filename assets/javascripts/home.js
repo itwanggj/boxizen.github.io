43e41e1592e41b5c858ee544d373c4a9375a9659
= <=>
2015-03-17 14:10:38 +0800

    更新样式,添加移动端适配

--- assets/javascripts/home.js
+++ assets/javascripts/home.js
@@ -1,6 +1,16 @@
 $(window).load(function(){
-	$(".portrait").fadeIn(1500,function(){
+	/*$(".portrait").fadeIn(1500,function(){
 			$(".portrait img").addClass("spin");
 			$(".showOff").fadeIn(500);
-		});
+		});*/
+	$('.hiddenMenu').click(function(){
+		if($(this).hasClass('toggled')){
+			$(this).removeClass('toggled');
+			$('.hiddenMenuList').addClass('hidden');
+		}
+		else{
+			$(this).addClass('toggled');
+			$('.hiddenMenuList').removeClass('hidden');
+		}
+	})
 });
